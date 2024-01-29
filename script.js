95fe551260ff28a23775eaf2efe7b6ba3e1391ff
Vikas Tripathi <vikastripathi9867@gmail.com>
2024-01-29 20:17:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,449 @@
+// For anchor-1
+
+var a = document.querySelector("#navbar li #anchor-1")
+var b = document.querySelector("#fixed-box")
+var c = document.querySelector(".items .item1")
+var d = document.querySelector(".items .item2")
+var e = document.querySelector(".items .item3")
+var f = document.querySelector(".items .item4")
+var g = document.querySelector(".items .item5")
+var h = document.querySelector(".items .item6")
+var i = document.querySelector(".items .item7")
+var j = document.querySelector(".items .item8")
+var k = document.querySelector(".items .item9")
+var l = document.querySelector(".items .item10")
+var m = document.querySelector(".items .item11")
+a.addEventListener("mouseenter",function(){
+    b.style.top = "0%"
+    b.style.height = "65vh"
+    c.style.display = "block"
+    d.style.display = "none"
+    c.style.display = "flex"
+    e.style.display = "none"
+    m.style.display = "none"
+    f.style.display = "none"
+    l.style.display = "none"
+    g.style.display = "none"
+    j.style.display = "none"
+    h.style.display = "none"
+    k.style.display = "none"
+    i.style.display = "none"
+})
+a.addEventListener("mouseleave",function(){
+    b.style.top = "-100%"
+
+})
+
+
+// For anchor-2
+
+var a = document.querySelector("#navbar li #anchor-2")
+var b = document.querySelector("#fixed-box")
+var c = document.querySelector(".items .item1")
+var d = document.querySelector(".items .item2")
+var e = document.querySelector(".items .item3")
+var f = document.querySelector(".items .item4")
+var g = document.querySelector(".items .item5")
+var h = document.querySelector(".items .item6")
+var i = document.querySelector(".items .item7")
+var j = document.querySelector(".items .item8")
+var k = document.querySelector(".items .item9")
+var l = document.querySelector(".items .item10")
+var m = document.querySelector(".items .item11")
+a.addEventListener("mouseenter",function(){
+    b.style.top = "0%"
+    b.style.height = "65vh"
+    c.style.display = "none"
+    d.style.display = "block"
+    d.style.display = "flex"
+    e.style.display = "none"
+    f.style.display = "none"
+    m.style.display = "none"
+    l.style.display = "none"
+    g.style.display = "none"
+    k.style.display = "none"
+    j.style.display = "none"
+    h.style.display = "none"
+    i.style.display = "none"
+
+})
+a.addEventListener("mouseleave",function(){
+    b.style.top = "-100%"
+})
+
+
+// For anchor-3
+
+var a = document.querySelector("#navbar li #anchor-3")
+var b = document.querySelector("#fixed-box")
+var c = document.querySelector(".items .item1")
+var d = document.querySelector(".items .item2")
+var e = document.querySelector(".items .item3")
+var f = document.querySelector(".items .item4")
+var g = document.querySelector(".items .item5")
+var h = document.querySelector(".items .item6")
+var i = document.querySelector(".items .item7")
+var j = document.querySelector(".items .item8")
+var k = document.querySelector(".items .item9")
+var l = document.querySelector(".items .item10")
+var m = document.querySelector(".items .item11")
+a.addEventListener("mouseenter",function(){
+    b.style.top = "0%"
+    b.style.height = "60vh"
+    c.style.display = "none"
+    d.style.display = "none"
+    l.style.display = "none"
+    e.style.display = "block"
+    e.style.display = "flex"
+    f.style.display = "none"
+    g.style.display = "none"
+    m.style.display = "none"
+    h.style.display = "none"
+    k.style.display = "none"
+    i.style.display = "none"
+    j.style.display = "none"
+    
+})
+a.addEventListener("mouseleave",function(){
+    b.style.top = "-100%"
+
+})
+
+
+// For anchor-4
+
+var a = document.querySelector("#navbar li #anchor-4")
+var b = document.querySelector("#fixed-box")
+var c = document.querySelector(".items .item1")
+var d = document.querySelector(".items .item2")
+var e = document.querySelector(".items .item3")
+var f = document.querySelector(".items .item4")
+var g = document.querySelector(".items .item5")
+var h = document.querySelector(".items .item6")
+var i = document.querySelector(".items .item7")
+var j = document.querySelector(".items .item8")
+var k = document.querySelector(".items .item9")
+var l = document.querySelector(".items .item10")
+var m = document.querySelector(".items .item11")
+a.addEventListener("mouseenter",function(){
+    b.style.top = "0%"
+    b.style.height = "55vh"
+    c.style.display = "none"
+    d.style.display = "none"
+    e.style.display = "none"
+    l.style.display = "none"
+    m.style.display = "none"
+    f.style.display = "block"
+    f.style.display = "flex"
+    g.style.display = "none"
+    k.style.display = "none"
+    j.style.display = "none"
+    i.style.display = "none"
+    h.style.display = "none"
+
+})
+a.addEventListener("mouseleave",function(){
+    b.style.top = "-100%"
+    
+})
+
+
+// For anchor-5
+
+var a = document.querySelector("#navbar li #anchor-5")
+var b = document.querySelector("#fixed-box")
+var c = document.querySelector(".items .item1")
+var d = document.querySelector(".items .item2")
+var e = document.querySelector(".items .item3")
+var f = document.querySelector(".items .item4")
+var g = document.querySelector(".items .item5")
+var h = document.querySelector(".items .item6")
+var i = document.querySelector(".items .item7")
+var j = document.querySelector(".items .item8")
+var k = document.querySelector(".items .item9")
+var l = document.querySelector(".items .item10")
+var m = document.querySelector(".items .item11")
+a.addEventListener("mouseenter",function(){
+    b.style.top = "0%"
+    b.style.height = "55vh"
+    c.style.display = "none"
+    d.style.display = "none"
+    e.style.display = "none"
+    f.style.display = "none"
+    i.style.display = "none"
+    j.style.display = "none"
+    m.style.display = "none"
+    l.style.display = "none"
+    g.style.display = "block"
+    g.style.display = "flex"
+    h.style.display = "none"
+    k.style.display = "none"
+
+})
+a.addEventListener("mouseleave",function(){
+    b.style.top = "-100%"
+    
+})
+
+
+// For anchor-6
+
+var a = document.querySelector("#navbar li #anchor-6")
+var b = document.querySelector("#fixed-box")
+var c = document.querySelector(".items .item1")
+var d = document.querySelector(".items .item2")
+var e = document.querySelector(".items .item3")
+var f = document.querySelector(".items .item4")
+var g = document.querySelector(".items .item5")
+var h = document.querySelector(".items .item6")
+var i = document.querySelector(".items .item7")
+var j = document.querySelector(".items .item8")
+var k = document.querySelector(".items .item9")
+var l = document.querySelector(".items .item10")
+var m = document.querySelector(".items .item11")
+a.addEventListener("mouseenter",function(){
+    b.style.top = "0%"
+    b.style.height = "38vh"
+    c.style.display = "none"
+    k.style.display = "none"
+    d.style.display = "none"
+    e.style.display = "none"
+    f.style.display = "none"
+    m.style.display = "none"
+    g.style.display = "none"
+    l.style.display = "none"
+    i.style.display = "none"
+    j.style.display = "none"
+    h.style.display = "block"
+    h.style.display = "flex"
+})
+a.addEventListener("mouseleave",function(){
+    b.style.top = "-100%"
+    
+})
+
+
+// For anchor-7
+
+var a = document.querySelector("#navbar li #anchor-7")
+var b = document.querySelector("#fixed-box")
+var c = document.querySelector(".items .item1")
+var d = document.querySelector(".items .item2")
+var e = document.querySelector(".items .item3")
+var f = document.querySelector(".items .item4")
+var g = document.querySelector(".items .item5")
+var h = document.querySelector(".items .item6")
+var i = document.querySelector(".items .item7")
+var j = document.querySelector(".items .item8")
+var k = document.querySelector(".items .item9")
+var l = document.querySelector(".items .item10")
+var m = document.querySelector(".items .item11")
+a.addEventListener("mouseenter",function(){
+    b.style.top = "0%"
+    b.style.height = "50vh"
+    c.style.display = "none"
+    d.style.display = "none"
+    m.style.display = "none"
+    e.style.display = "none"
+    f.style.display = "none"
+    g.style.display = "none"
+    l.style.display = "none"
+    h.style.display = "none"
+    j.style.display = "none"
+    k.style.display = "none"
+    i.style.display = "block"
+    i.style.display = "flex"
+})
+a.addEventListener("mouseleave",function(){
+    b.style.top = "-100%"
+    
+})
+
+
+// For anchor-8
+
+var a = document.querySelector("#navbar li #anchor-8")
+var b = document.querySelector("#fixed-box")
+var c = document.querySelector(".items .item1")
+var d = document.querySelector(".items .item2")
+var e = document.querySelector(".items .item3")
+var f = document.querySelector(".items .item4")
+var g = document.querySelector(".items .item5")
+var h = document.querySelector(".items .item6")
+var i = document.querySelector(".items .item7")
+var j = document.querySelector(".items .item8")
+var k = document.querySelector(".items .item9")
+var l = document.querySelector(".items .item10")
+var m = document.querySelector(".items .item11")
+a.addEventListener("mouseenter",function(){
+    b.style.top = "0%"
+    b.style.height = "65vh"
+    c.style.display = "none"
+    d.style.display = "none"
+    e.style.display = "none"
+    f.style.display = "none"
+    m.style.display = "none"
+    g.style.display = "none"
+    h.style.display = "none"
+    l.style.display = "none"
+    i.style.display = "none"
+    k.style.display = "none"
+    j.style.display = "block"
+    j.style.display = "flex"
+})
+a.addEventListener("mouseleave",function(){
+    b.style.top = "-100%"
+    
+})
+
+
+// For anchor-9
+
+var a = document.querySelector("#navbar li #anchor-9")
+var b = document.querySelector("#fixed-box")
+var c = document.querySelector(".items .item1")
+var d = document.querySelector(".items .item2")
+var e = document.querySelector(".items .item3")
+var f = document.querySelector(".items .item4")
+var g = document.querySelector(".items .item5")
+var h = document.querySelector(".items .item6")
+var i = document.querySelector(".items .item7")
+var j = document.querySelector(".items .item8")
+var k = document.querySelector(".items .item9")
+var l = document.querySelector(".items .item10")
+var m = document.querySelector(".items .item11")
+a.addEventListener("mouseenter",function(){
+    b.style.top = "0%"
+    b.style.height = "73vh"
+    c.style.display = "none"
+    d.style.display = "none"
+    e.style.display = "none"
+    m.style.display = "none"
+    f.style.display = "none"
+    g.style.display = "none"
+    h.style.display = "none"
+    i.style.display = "none"
+    l.style.display = "none"
+    j.style.display = "none"
+    k.style.display = "block"
+    k.style.display = "flex"
+})
+a.addEventListener("mouseleave",function(){
+    b.style.top = "-100%"
+    
+})
+
+
+
+// For anchor-10
+
+var a = document.querySelector("#navbar li #anchor-10")
+var b = document.querySelector("#fixed-box")
+var c = document.querySelector(".items .item1")
+var d = document.querySelector(".items .item2")
+var e = document.querySelector(".items .item3")
+var f = document.querySelector(".items .item4")
+var g = document.querySelector(".items .item5")
+var h = document.querySelector(".items .item6")
+var i = document.querySelector(".items .item7")
+var j = document.querySelector(".items .item8")
+var k = document.querySelector(".items .item9")
+var l = document.querySelector(".items .item10")
+var m = document.querySelector(".items .item11")
+a.addEventListener("mouseenter",function(){
+
+    b.style.top = "0%"
+    b.style.height = "65vh"
+    c.style.display = "none"
+    d.style.display = "none"
+    e.style.display = "none"
+    f.style.display = "none"
+    g.style.display = "none"
+    m.style.display = "none"
+    h.style.display = "none"
+    i.style.display = "none"
+    j.style.display = "none"
+    k.style.display = "none"
+    l.style.display = "block"
+    l.style.display = "flex"
+})
+a.addEventListener("mouseleave",function(){
+    b.style.top = "-100%"
+    
+})
+
+
+
+// For anchor-11
+
+var a = document.querySelector("#navbar li #anchor-11")
+var b = document.querySelector("#fixed-box")
+var c = document.querySelector(".items .item1")
+var d = document.querySelector(".items .item2")
+var e = document.querySelector(".items .item3")
+var f = document.querySelector(".items .item4")
+var g = document.querySelector(".items .item5")
+var h = document.querySelector(".items .item6")
+var i = document.querySelector(".items .item7")
+var j = document.querySelector(".items .item8")
+var k = document.querySelector(".items .item9")
+var l = document.querySelector(".items .item10")
+var m = document.querySelector(".items .item11")
+a.addEventListener("mouseenter",function(){
+
+    b.style.top = "0%"
+    b.style.height = "65vh"
+    c.style.display = "none"
+    d.style.display = "none"
+    e.style.display = "none"
+    f.style.display = "none"
+    g.style.display = "none"
+    h.style.display = "none"
+    i.style.display = "none"
+    j.style.display = "none"
+    k.style.display = "none"
+    l.style.display = "none"
+    m.style.display = "block"
+    m.style.display = "flex"
+})
+a.addEventListener("mouseleave",function(){
+    b.style.top = "-100%"
+    
+})
+
+
+// functioning the navbar's background into black color whenever cursor pointer point towards it.
+
+
+var pp = document.querySelectorAll("#navbar a")
+var gg = document.querySelector("#navbar")
+pp.forEach(function(e)
+{
+    e.addEventListener("mouseenter",function(){
+        gg.style.backgroundColor = "black"
+    })
+    e.addEventListener("mouseleave",function()
+    {
+        gg.style.backgroundColor = "#434344"
+    })
+})
+
+
+// Swiper part
+
+
+    var swiper = new    Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 10,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+          autoplay: {
+      delay:5000,
+      disableOnInteraction:false,
+    },
+      });
+
+
